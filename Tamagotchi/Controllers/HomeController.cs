dedfb9ea7e10dde4080ed8d4caba94cfb61fc06a
Stephen Trewick and Zema Kebede <stephen+zema@>
2019-05-01 15:53:33 -0700

    index controller puts pet name on page

--- Tamagotchi/Controllers/HomeController.cs
+++ Tamagotchi/Controllers/HomeController.cs
@@ -1,2 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Tamagotchi.Models;
+
+namespace Tamagotchi.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    public ActionResult Index()
+    {
+      Pet newPet = new Pet("Panatda", 5, 5, 5);
+      return View(newPet);
+    }
+  }
+}
